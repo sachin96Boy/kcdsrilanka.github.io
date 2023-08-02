bf87804c61ac026d77adc90bc5226ec6e65c8f9c
Akalanka <akalankaperera128@gmail.com>
2023-08-02 09:55:15 +0530

    Fix: workflow base path update step

--- src/components/organizers/index.svelte
+++ src/components/organizers/index.svelte
@@ -4,7 +4,7 @@
 	import organizers from './data';
 </script>
 
-<div class="flex flex-col pt-20 py-24 section-container gap-y-16">
+<div class="flex flex-col pt-20 py-24 section-container gap-y-16 relative z-[2]">
 	<Heading>Organizers</Heading>
 	<div
 		class="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-[30px]"
