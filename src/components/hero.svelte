16ddbb36f43eb2630de9896953baf5ec6a7a42d1
Akalanka <akalankaperera128@gmail.com>
2023-08-02 10:13:35 +0530

    Fix: hero text

--- src/components/hero.svelte
+++ src/components/hero.svelte
@@ -6,6 +6,6 @@
 	<KCDLarge class="animate-in fade-in duration-700" />
 	<span class="flex flex-col gap-4 text-center font-bold text-5xl md:text-[56px]">
 		<span class="animate-in slide-in-from-left duration-700"> Kubernetes Community Days </span>
-		<span class="animate-in slide-in-from-right duration-700"> Sri Lanka </span>
+		<span class="animate-in slide-in-from-right duration-700"> Sri Lanka 2023 </span>
 	</span>
 </div>
