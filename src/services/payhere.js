04b6883b3fea3abb1179ee8e38d6ae34242cb94f
Akalanka <akalankaperera128@gmail.com>
2023-08-05 02:10:11 +0530

    Feat: integrated payhere

---
+++ src/services/payhere.js
@@ -0,0 +1,46 @@
+import { browser } from '$app/environment';
+import { PUBLIC_FRONTEND_URL } from '$env/static/public';
+import { default as toast } from 'svelte-french-toast';
+import { cancelPayment, verifyPayment } from './ticket';
+
+if (browser) {
+	window.payhere.onCompleted = function onCompleted() {
+		verifyPayment().then((res) => {
+			if (res?.data && res.data?.paymentStatus === 'success') {
+				toast.success('Payment successful');
+			} else {
+				toast.error('Payment failed');
+			}
+		});
+	};
+
+	window.payhere.onDismissed = function onDismissed() {
+		cancelPayment();
+	};
+
+	window.payhere.onError = function onError(error) {
+		console.log('Payment Error', error);
+	};
+}
+
+export const processPayment = (user, data) => {
+	window.payhere.startPayment({
+		sandbox: !data.isProduction,
+		merchant_id: data.merchantId,
+		return_url: PUBLIC_FRONTEND_URL,
+		cancel_url: PUBLIC_FRONTEND_URL,
+		notify_url: PUBLIC_FRONTEND_URL,
+		order_id: data.reference,
+		items: 'KCDSL Ticket',
+		amount: data.cost.toFixed(2),
+		currency: 'LKR',
+		hash: data.hash,
+		first_name: user.name?.includes(' ') ? user.name.split(' ')[0] : user.name,
+		last_name: user.name?.includes(' ') ? user.name.split(' ')[1] : 'N/A',
+		email: user.email,
+		phone: user.mobile,
+		address: 'AC19, Colombo 01000',
+		city: 'Colombo',
+		country: 'Sri Lanka'
+	});
+};
--- src/services/ticket.js
+++ src/services/ticket.js
@@ -1,11 +1,14 @@
 import { default as toast } from 'svelte-french-toast';
 import axios from './base';
+import { processPayment } from './payhere';
 
 export const initiatePayment = (registeredUser, v = 'v1') => {
 	if (!registeredUser) {
 		return toast.error('Please login to continue');
 	}
-	return axios.post(`/${v}/tickets/payments/initiate`);
+	return axios
+		.post(`/${v}/tickets/payments/initiate`)
+		.then((res) => processPayment(registeredUser, res.data));
 };
 
 export const verifyPayment = (v = 'v1') => axios.get(`/${v}/tickets/payments/verify`);
